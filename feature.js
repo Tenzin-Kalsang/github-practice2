c1a46522d94df1a775f56826172fb407f64565f7
Tenzin <tznklsng28@gmail.com>
2021-04-17 18:07:13 -0400

    some javascript

---
+++ feature.js
@@ -0,0 +1,7 @@
+function hello(){
+    if(document.querySelector('h1').innerHTML === 'Hello!'){
+        document.querySelector('h1').innerHTML = 'Goodbye!';
+    }else{
+        document.querySelector('h1').innerHTML = 'Hello!';
+    }
+}
